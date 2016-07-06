ea6e078360f2280f99b194fbb6541b05482aff9f
Lajos Köblös <lajos.koblos@ge.com>
2016-07-06 15:45:45 +0200

    fix after merge

--- index.js
+++ index.js
@@ -1,44 +1,43 @@
 angular.module("contactServiceModule", ["authServiceModule"])
 
-.factory("contactService", function (authService, $q) {
-    var contactServiceObject = {};
+        var contactServiceObject = {};
 
-    contactServiceObject.getContact = function(groupId, contactId) {
-        var deferred = $q.defer();
-        deferred.resolve({
-            "firstName":"firstName",
-            "lastName":"lastName",
-            "workEmail":"work@email.email",
-            "homeEmail":"home@email.email",
-            "nickName":"nickName",
-            "jobTitle":"jobTitle",
-            "_links":{"self":{"href":"http://localhost/groups/name/contacts/1"}}
-        });
-        return deferred.promise;
-    };
+        contactServiceObject.getContact = function (groupId, contactId) {
+            var deferred = $q.defer();
+            deferred.resolve({
+                "firstName": "firstName",
+                "lastName": "lastName",
+                "workEmail": "work@email.email",
+                "homeEmail": "home@email.email",
+                "nickName": "nickName",
+                "jobTitle": "jobTitle",
+                "_links": {"self": {"href": "http://localhost/groups/name/contacts/1"}}
+            });
+            return deferred.promise;
+        };
 
-    contactServiceObject.getContactByExample = function(groupId, contact) {
-        var deferred = $q.defer();
-        deferred.resolve([
-            {
-                "firstName":"firstName",
-                "lastName":"lastName",
-                "workEmail":"work@email.email",
-                "nickName":"nickName",
-                "jobTitle":"jobTitle",
-                "_links":{"self":{"href":"http://localhost/groups/name/contacts/1"}}
-            },
-            {
-                "firstName":"firstName",
-                "lastName":"lastName",
-                "workEmail":"work@email.email",
-                "nickName":"nickName",
-                "jobTitle":"jobTitle",
-                "_links":{"self":{"href":"http://localhost/groups/name/contacts/2"}}
-            }
-        ]);
-        return deferred.promise;
-    };
+        contactServiceObject.getContactByExample = function (groupId, contact) {
+            var deferred = $q.defer();
+            deferred.resolve([
+                {
+                    "firstName": "firstName",
+                    "lastName": "lastName",
+                    "workEmail": "work@email.email",
+                    "nickName": "nickName",
+                    "jobTitle": "jobTitle",
+                    "_links": {"self": {"href": "http://localhost/groups/name/contacts/1"}}
+                },
+                {
+                    "firstName": "firstName",
+                    "lastName": "lastName",
+                    "workEmail": "work@email.email",
+                    "nickName": "nickName",
+                    "jobTitle": "jobTitle",
+                    "_links": {"self": {"href": "http://localhost/groups/name/contacts/2"}}
+                }
+            ]);
+            return deferred.promise;
+        };
 
         contactServiceObject.getContactsInGroup = function (groupId) {
             var deferred = $q.defer();
@@ -73,29 +72,29 @@ angular.module("contactServiceModule", ["authServiceModule"])
             return deferred.promise;
         };
 
-    contactServiceObject.addContactToGroup = function(groupId, contact) {
-        var deferred = $q.defer();
-        deferred.resolve({});
-        return deferred.promise;
-    };
+        contactServiceObject.addContactToGroup = function (groupId, contact) {
+            var deferred = $q.defer();
+            deferred.resolve({});
+            return deferred.promise;
+        };
 
-    contactServiceObject.updateContact = function(groupId, contact) {
-        var deferred = $q.defer();
-        deferred.resolve({});
-        return deferred.promise;
-    };
+        contactServiceObject.updateContact = function (groupId, contact) {
+            var deferred = $q.defer();
+            deferred.resolve({});
+            return deferred.promise;
+        };
 
-    contactServiceObject.deleteContact = function(groupId, contactId) {
-        var deferred = $q.defer();
-        deferred.resolve();
-        return deferred.promise;
-    };
+        contactServiceObject.deleteContact = function (groupId, contactId) {
+            var deferred = $q.defer();
+            deferred.resolve();
+            return deferred.promise;
+        };
 
-    contactServiceObject.validateContact = function(contact) {
-        var deferred = $q.defer();
-        deferred.resolve({});
-        return deferred.promise;
-    };
+        contactServiceObject.validateContact = function (contact) {
+            var deferred = $q.defer();
+            deferred.resolve({});
+            return deferred.promise;
+        };
 
-    return contactServiceObject;
-});
+        return contactServiceObject;
+    });
