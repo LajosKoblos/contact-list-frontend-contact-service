d09ca4988e613f7a9c8adbfcd4f372e60fb886e1
Lajos Köblös <lajos.koblos@ge.com>
2016-07-06 14:46:58 +0200

    imlementation and test

--- index.js
+++ index.js
@@ -1,91 +1,99 @@
 angular.module("contactServiceModule", ["authServiceModule"])
 
-.factory("contactService", function (authService, $q) {
-    var contactServiceObject = {};
+    .factory("contactService", function ($httpWithProtection, $q) {
+        var contactServiceObject = {};
 
-    contactServiceObject.getContact = function(groupId, contactId) {
-        var deferred = $q.defer();
-        deferred.resolve({
-            "firstName":"firstName",
-            "lastName":"lastName",
-            "workEmail":"work@email.email",
-            "homeEmail":"home@email.email",
-            "nickName":"nickName",
-            "jobTitle":"jobTitle",
-            "_links":{"self":{"href":"http://localhost/groups/name/contacts/1"}}
-        });
-        return deferred.promise;
-    };
+        contactServiceObject.getContact = function (groupId, contactId) {
+            var deferred = $q.defer();
+            deferred.resolve({
+                "firstName": "firstName",
+                "lastName": "lastName",
+                "workEmail": "work@email.email",
+                "homeEmail": "home@email.email",
+                "nickName": "nickName",
+                "jobTitle": "jobTitle",
+                "_links": {"self": {"href": "http://localhost/groups/name/contacts/1"}}
+            });
+            return deferred.promise;
+        };
 
-    contactServiceObject.getContactByExample = function(groupId, contact) {
-        var deferred = $q.defer();
-        deferred.resolve([
-            {
-                "firstName":"firstName",
-                "lastName":"lastName",
-                "workEmail":"work@email.email",
-                "nickName":"nickName",
-                "jobTitle":"jobTitle",
-                "_links":{"self":{"href":"http://localhost/groups/name/contacts/1"}}
-            },
-            {
-                "firstName":"firstName",
-                "lastName":"lastName",
-                "workEmail":"work@email.email",
-                "nickName":"nickName",
-                "jobTitle":"jobTitle",
-                "_links":{"self":{"href":"http://localhost/groups/name/contacts/1"}}
+        contactServiceObject.getContactByExample = function (groupId, contact) {
+            var deferred = $q.defer();
+            deferred.resolve([
+                {
+                    "firstName": "firstName",
+                    "lastName": "lastName",
+                    "workEmail": "work@email.email",
+                    "nickName": "nickName",
+                    "jobTitle": "jobTitle",
+                    "_links": {"self": {"href": "http://localhost/groups/name/contacts/1"}}
+                },
+                {
+                    "firstName": "firstName",
+                    "lastName": "lastName",
+                    "workEmail": "work@email.email",
+                    "nickName": "nickName",
+                    "jobTitle": "jobTitle",
+                    "_links": {"self": {"href": "http://localhost/groups/name/contacts/1"}}
+                }
+            ]);
+            return deferred.promise;
+        };
+
+        contactServiceObject.getContactsInGroup = function (groupId) {
+            var deferred = $q.defer();
+
+            if (typeof groupId === "undefined") {
+                deferred.reject({
+                    message: "Argument Error",
+                    fields: {
+                        groupId: ["groupId is reqired"]
+                    }
+                });
+                return deferred.promise;
             }
-        ]);
-        return deferred.promise;
-    };
 
-    contactServiceObject.getContactsInGroup = function(groupId) {
-        var deferred = $q.defer();
-        deferred.resolve([
-            {
-                "firstName":"firstName",
-                "lastName":"lastName",
-                "workEmail":"work@email.email",
-                "nickName":"nickName",
-                "jobTitle":"jobTitle",
-                "_links":{"self":{"href":"http://localhost/groups/name/contacts/1"}}
-            },
-            {
-                "firstName":"firstName",
-                "lastName":"lastName",
-                "workEmail":"work@email.email",
-                "nickName":"nickName",
-                "jobTitle":"jobTitle",
-                "_links":{"self":{"href":"http://localhost/groups/name/contacts/1"}}
-            }
-        ]);
-        return deferred.promise;
-    };
+            var config = {
+                url: "http://localhost:8080/groups/" + groupId + "/contacts",
+                method: "GET"
+            };
 
-    contactServiceObject.addContactToGroup = function(groupId, contact) {
-        var deferred = $q.defer();
-        deferred.resolve({});
-        return deferred.promise;
-    };
+            var httpPromise = $httpWithProtection(config);
 
-    contactServiceObject.updateContact = function(groupId, contact) {
-        var deferred = $q.defer();
-        deferred.resolve({});
-        return deferred.promise;
-    };
+            httpPromise.then(function (data) {
+                deferred.resolve(data);
+            }, function (error) {
+                deferred.reject({
+                
+                });
+            });
 
-    contactServiceObject.deleteContact = function(groupId, contactId) {
-        var deferred = $q.defer();
-        deferred.resolve();
-        return deferred.promise;
-    };
+            return deferred.promise;
+        };
 
-    contactServiceObject.validateContact = function(contact) {
-        var deferred = $q.defer();
-        deferred.resolve({});
-        return deferred.promise;
-    };
+        contactServiceObject.addContactToGroup = function (groupId, contact) {
+            var deferred = $q.defer();
+            deferred.resolve({});
+            return deferred.promise;
+        };
 
-    return contactServiceObject;
-});
+        contactServiceObject.updateContact = function (groupId, contact) {
+            var deferred = $q.defer();
+            deferred.resolve({});
+            return deferred.promise;
+        };
+
+        contactServiceObject.deleteContact = function (groupId, contactId) {
+            var deferred = $q.defer();
+            deferred.resolve();
+            return deferred.promise;
+        };
+
+        contactServiceObject.validateContact = function (contact) {
+            var deferred = $q.defer();
+            deferred.resolve({});
+            return deferred.promise;
+        };
+
+        return contactServiceObject;
+    });
--- test/getContactsInGroupTest.js
+++ test/getContactsInGroupTest.js
@@ -1,13 +1,54 @@
-describe("getContactsInGroup suite", function() {
-    var authServiceMock = {};
-    var $httpWithProtectionMock = {};
+describe("getContactsInGroup method", function () {
+    var $httpBackend, $rootScope, _contactService;
 
-    beforeEach(module("contactServiceModule", function($provide){
-        $provide.value("authService", authServiceMock);
-        $provide.value("$httpWithProtection", $httpWithProtectionMock);
-    }));
+    beforeEach(function () {
+        module("contactServiceModule", function ($provide, $httpProvider) {
+            $provide.provider("$httpWithProtectionProvider", $httpProvider);
+        });
 
-    it("contains spec with an expectation", function() {
-        expect(true).toBe(true);
+        inject(function (_$rootScope_, _$httpBackend_, contactService) {
+            $rootScope = _$rootScope_;
+            $httpBackend = _$httpBackend_;
+            _contactService = contactService;
+        });
+    });
+
+    afterEach(function () {
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    it("should return the expected list of contacts", function () {
+        var contactList = [];
+
+        $httpBackend.expectGET("http://localhost:8080/groups/0/contacts");
+
+        $httpBackend.when("GET", "http://localhost:8080/groups/0/contacts").respond(contactList);
+
+        var promise = _contactService.getContactsInGroup(0);
+
+        promise.then(function (data) {
+            expect(data.data).toEqual(contactList);
+        });
+
+        $rootScope.$apply();
+
+        $httpBackend.flush();
+    });
+
+    it("should return an error object with argument error message", function () {
+        var promise = _contactService.getContactsInGroup();
+
+        promise.catch(function (reason) {
+            expect(reason).toEqual({
+                message: "Argument Error",
+                fields: {
+                    groupId: ["groupId is reqired"]
+                }
+            });
+        });
+
+        $rootScope.$apply();
+
     });
 });
